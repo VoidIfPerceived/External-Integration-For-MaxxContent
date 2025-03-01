15a77c9621098398943d0dfec5fc7cd5ca91c04b
VoidIfPerceived <sophi@offnominal.com>
2025-03-01 00:01:48 -0600

    Adjusted several files, filenames, restarted building activity plugin instead of lci/source plugin

---
+++ version.php
@@ -0,0 +1,8 @@
+<?php
+
+defined('MOODLE_INTERNAL') || die();
+
+$plugin->version = 2025022100; // Date of latest version of module (Date: YYYYMMDDXX)
+$plugin->component = 'mod_extintmaxx'; // Full name of the plugin
+$plugin->release = '0.1'; // Current readable version number (as seen on the page: Plugins/Plugins Overview/)
+$plugin->maturity = MATURITY_ALPHA; // Current stability of plugin
