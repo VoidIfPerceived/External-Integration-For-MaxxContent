c264af65d6873d5e09c8b16dc54727843242ffe6
VoidIfPerceived <sophi@offnominal.com>
2025-04-23 09:41:16 -0500

    04/23/25 -- Edited plugin version number

--- version.php
+++ version.php
@@ -2,7 +2,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2025042100; // Date of latest version of module (Date: YYYYMMDDXX)
+$plugin->version = 2025042101; // Date of latest version of module (Date: YYYYMMDDXX)
 $plugin->component = 'mod_extintmaxx'; // Full name of the plugin
 $plugin->release = '1.0'; // Current readable version number (as seen on the page: Plugins/Plugins Overview/)
 $plugin->maturity = MATURITY_RC; // Current stability of plugin
